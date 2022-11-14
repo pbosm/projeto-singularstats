0443c7eaa3f20643644890acd7ce75c435f9b4ed
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2022-11-14 17:46:00 -0300

    class connection

--- connection.php
+++ connection.php
@@ -5,25 +5,20 @@ class Database
     protected static $db;
     private function __construct()
     {
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"],1);
-        $active_group = 'default';
-        $query_builder = TRUE;
-        // // Connect to DB
-        // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-        // // $db_host = 'localhost';
-        // // $db_user = 'root';
-        // // $db_password = '';
-        // // $db_name = 'bdlolcblol';
-        // // $db_driver = "mysql";
-        $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
-      
+
         try
         {
-            self::$db = new PDO("$db_driver:host=$cleardb_server; dbname=$cleardb_db", $cleardb_username, $cleardb_password, $options);
+            $db = parse_url(getenv("DATABASE_URL"));
+
+            $pdo = new PDO("pgsql:" . sprintf(
+                "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                $db["host"],
+                $db["port"],
+                $db["user"],
+                $db["pass"],
+                ltrim($db["path"], "/")
+            ));
+            
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // self::$db->exec('SET NAMES utf8');
             self::$db->exec('SET NAMES utf8mb4');
