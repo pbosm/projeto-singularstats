8eb755c692fa693503fc595fd1f8e28e91594ebf
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2022-11-14 18:42:13 -0300

    class connection

--- connection.php
+++ connection.php
@@ -5,16 +5,15 @@ class Database
     protected static $db;
     private function __construct()
     {
-        $url = parse_url(getenv("mysql://b16d12bb28e769:07d75b41@us-cdbr-east-06.cleardb.net/heroku_4fbca0687799a5f?reconnect=true"));
-
-        $servername     = $url["host"];
-        $username       = $url["user"];
-        $password       = $url["pass"];
-        $dbname         = substr($url["path"], 1);
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"],1);
 
         try
         {
-            self::$db = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+            self::$db = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // self::$db->exec('SET NAMES utf8');
             self::$db->exec('SET NAMES utf8mb4');
