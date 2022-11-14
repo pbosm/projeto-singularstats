13776b440786801cba8b1caaf1b37f5a562fb563
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2022-11-14 18:10:01 -0300

    class connection

--- connection.php
+++ connection.php
@@ -2,8 +2,8 @@
 
 class Database 
 {
-    protected static $db;
-    private function __construct()
+
+    public static function connectionPDO()
     {
         $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $cleardb_server = $cleardb_url["host"];
@@ -12,27 +12,8 @@ class Database
         $cleardb_db = substr($cleardb_url["path"],1);
         $active_group = 'default';
         $query_builder = TRUE;
-        $options = array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );
-      
-        try
-        {
-            self::$db = new PDO("$db_driver:host=$cleardb_server; dbname=$cleardb_db", $cleardb_url, $cleardb_username, $cleardb_password, $options, $query_builder, $active_group);
-            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // self::$db->exec('SET NAMES utf8');
-            self::$db->exec('SET NAMES utf8mb4');
-        }
-        catch (PDOException $e)
-        {   
-            die("Connection Error: " . $e->getMessage());
-        }
-    }
-    public static function connectionPDO()
-    {
-        if (!self::$db)
-        {
-            new Database();
-        }
-        return self::$db;
+        // Connect to DB
+        $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     }
 }
 
