232c2e6633a59f50773489c73eb7851a00177748
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-01-04 11:46:39 -0300

    atualizando front

--- assets/headerIndex.php
+++ assets/headerIndex.php
@@ -9,8 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="icon" href="./image/icone.png">
-    <!-- <link rel="stylesheet" type="text/css" href="./css/style.css"> -->
-    <style>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!-- <style> -->
         @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');
 
         * {
