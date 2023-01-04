b6839f67181f5397c241831c671e258568d5b4e9
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-01-04 09:49:02 -0300

    atualizando front

--- assets/headerIndex.php
+++ assets/headerIndex.php
@@ -10,8 +10,8 @@
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-    <link rel="icon" href="./image/icone.png">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="icon" href="../image/icone.png">
 </head>
 
 <body>
