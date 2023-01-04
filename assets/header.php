26b747eeff632ac9f7878250df795d6c484d5e16
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-01-04 10:30:51 -0300

    atualizando front

--- assets/header.php
+++ assets/header.php
@@ -16,12 +16,14 @@ if($url_title == 'busca/buscacham') {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= ucfirst($url_title) ?> </title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="icon" href="./image/icone.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
