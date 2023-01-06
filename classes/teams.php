a0812003067a73ae2f5424c475d97e7b40d3e41a
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-01-06 15:42:33 -0300

    passando URL atráves dos parametros para pegar nome do time

--- classes/teams.php
+++ classes/teams.php
@@ -83,7 +83,7 @@
             $conn = Database::connectionPDO();
 
             $sql = "SELECT teamname, count(teamname), sum(duracaogame) / count(teamname) / 60, Sum(side='Blue'), sum(result=1 and side='Blue') / sum(side='blue') * 100,  Sum(side='Red'), sum(result=1 and side='red') / sum(side='red') * 100, SUM(result=1) / count(teamname) * 100, sum(firsttower) / count(teamname) * 100, sum(firsttower=1 and side='Blue') / sum(side='blue') * 100, sum(firsttower=1 and side='red') / sum(side='red') * 100, sum(firstblood) / count(teamname) * 100 from `cblol`
-            where split in (select split from `cblol` where split = 'split 2')
+            where split in (select split from `cblol` where split = 'split 1')
             and position in (select position from `cblol` where position = 'team')
             GROUP BY teamname";
             $code = $conn->prepare($sql, array());
