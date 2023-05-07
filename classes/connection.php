290e45e46254522e9b4bd3ce5ce74772d2f1746d
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-05-07 01:38:04 -0300

    new database connection

---
+++ classes/connection.php
@@ -0,0 +1,46 @@
+<?php
+
+class Database 
+{
+    protected static $db;
+    private function __construct()
+    {
+        //deploy herouku
+        // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        // $cleardb_server = $cleardb_url["host"];
+        // $cleardb_username = $cleardb_url["user"];
+        // $cleardb_password = $cleardb_url["pass"];
+        // $cleardb_db = substr($cleardb_url["path"],1);
+
+        $host     = "containers-us-west-195.railway.app";
+        $username = "root";
+        $pass     = "qGrq84De7I3FqSvDK0Ir";
+        $port     = "7704";
+        $db       = "railway";
+
+        $connecthost = "mysql:host=$host;dbname=$db;port=$port";
+        
+        try
+        {
+            self::$db = new \PDO($connecthost, $username, $pass);
+            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // self::$db->exec('SET NAMES utf8');
+            self::$db->exec('SET NAMES utf8mb4');
+            self::$db->exec("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
+        }
+        catch (PDOException $e)
+        {   
+            die("Connection Error: " . $e->getMessage());
+        }
+    }
+    public static function connectionPDO()
+    {
+        if (!self::$db)
+        {
+            new Database();
+        }
+        return self::$db;
+    }
+}
+
+?>
